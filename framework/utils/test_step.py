c38dc3031a9474cf116f5a230162cae81dd77517
22nd <vitaliiseptember@gmail.com>
2025-10-10 13:16:28 +0300

    tests allure + logger fixed small issued with get_driver - added param by default for chrome driver

--- framework/utils/test_step.py
+++ framework/utils/test_step.py
@@ -1,25 +1,16 @@
-import allure
-from framework.logger import AllureLogger
+from contextlib import ContextDecorator
 
-from framework.browser.browser_manager import BrowserManager
-
-logger = AllureLogger.get_test_logger(__name__)
-
-class TestStep:
-    def __init__(self, test_step_desctiption: str):
-        self.test_step_desctiption = test_step_desctiption
+class TestStep(ContextDecorator):
+    def __init__(self, name):
+        self.name = name
 
     def __enter__(self):
-        allure.dynamic.description(f"Step: {self.test_step_desctiption}")
-        logger.info(f"START: {self.test_step_desctiption}")
-        allure.step(self.test_step_desctiption)
-        return self
+        print(f"\nStart step: {self.name}")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type:
-            logger.error(f"FAILED: {self.test_step_desctiption} — {exc_val}")
-            BrowserManager.get_driver().save_screenshot(f"screens/{self.test_step_desctiption}.png")
+            print(f"Failed Step: {self.name} ")
+            print(f"Error: {exc_val}")
         else:
-            logger.info(f"PASSED: {self.test_step_desctiption}")
-        logger.info(f"END: {self.test_step_desctiption}")
+            print(f"PASSED STEP: {self.name}")
         return False
--- project/tests/tests.py
+++ project/tests/tests.py
@@ -1,11 +1,11 @@
 import time
 
 from framework.page.main_page import MainPage
-from framework.utils.test_step import TestStep as test_step
+from framework.utils.test_step import TestStep
 
 
 def test_main_page_title(driver):
-    with test_step("1. Check that the title is correct"):
+    with TestStep("1. Check that the title is correct"):
         assert driver.title == "Playground for the QA automation engineers"
 
 
@@ -13,8 +13,8 @@ def test_iframe_box_is_exist(driver):
     # page object
     main_page = MainPage()
 
-    with test_step("1. Click iframe box"):
+    with TestStep("1. Click iframe box"):
         main_page.click.click_iframe_box()
 
-    with test_step("2. Verify page URL contains 'iframe'"):
+    with TestStep("2. Verify page URL contains 'iframe'"):
         assert "iframe" in driver.current_url
