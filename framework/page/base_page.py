9827c22112c1ebbafa34a447c34095db23d3e598
22nd <vitaliiseptember@gmail.com>
2025-10-11 15:35:16 +0300

    added - is_opened

--- framework/page/base_page.py
+++ framework/page/base_page.py
@@ -1,6 +1,8 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 from framework.browser.browser_manager import BrowserManager
 
@@ -15,9 +17,13 @@ class BasePage(ABC):
     def driver(self):
         return BrowserManager().get_driver()
 
-    @abstractmethod
     def is_opened(self):
-        pass
+        try:
+            self._wait.until(EC.presence_of_element_located((self._by, self._locator)))
+        except TimeoutException as e:
+            print(f"Timed out {e}")
+            return False
+        return True
 
     def execute_script(self, script: str):
         return self.driver.execute_script(script, self.find_element())
@@ -25,6 +31,12 @@ class BasePage(ABC):
     def screenshot(self, path):
         self.find_element().screenshot(path)
 
-
     def refresh_page(self):
         self.driver.refresh()
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    @staticmethod
+    def get_window_size():
+        return BrowserManager.get_driver().get_window_size()
